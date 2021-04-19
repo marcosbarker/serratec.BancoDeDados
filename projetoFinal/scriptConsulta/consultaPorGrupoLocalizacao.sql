4ff7453ffe279de25d5d2df88667cd2a8bf00a65
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-04-19 16:58:00 -0300

    docs: add scriptConsulta

--- projetoFinal/scriptConsulta/consultaPorGrupoLocalizacao.sql
+++ projetoFinal/scriptConsulta/consultaPorGrupoLocalizacao.sql
@@ -1,6 +1,6 @@
 -- Group By Usuario Por Localização
 
-select e.bairro, count(usuario.id_usuario) as "Usuario por localizção"
+select endereco.bairro, count(usuario.id_usuario) as "Usuario por localização"
 from usuario 
 join endereco 
 on usuario.id_endereco = endereco.id_endereco
